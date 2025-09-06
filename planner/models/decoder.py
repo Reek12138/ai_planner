599079f666ab0bf5e64e5e8216afb1b6247418f4
Reek12138 <2894487027@qq.com>
2025-09-06 22:38:17 +0800

    first commit

---
+++ planner/models/decoder.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from typing import Dict, Any, Optional, Tuple, List
+import math
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+class TRAJ_Decoder(nn.Module):
+    def __init__(self, inpit_dim: int):
+        super().__init__()
+
+        pass
+
+    def forward(self, x):
+
+        return x
